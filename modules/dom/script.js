2cbebc05e0d77ad6ba94fe3f40fde74f223bb485
vrinceanuv <vrinceanu.valentin@gmail.com>
2017-01-23 17:55:47 +0000

    Added more functionality to the DOM function module

--- modules/dom/script.js
+++ modules/dom/script.js
@@ -2,38 +2,57 @@
 
   var dom;
 
-  function creatElement (element) {
+  function addElement (element) {
     return document.creatElement(element);
   }
 
-  function appendElement (element, elementToAppend) {
-    return elementToAppend.appendChild(element);
+  function removeElement (element) {
+    return element.parentNode.removeChild(element);
+  }
+
+  function append (element, parent) {
+    return parent.appendChild(element);
+  }
+
+  function prepend (element, parent) {
+    return parent.insertBefore(element, parent.firstChild);
   }
 
   function removeElement (element) {
-
+    return element.parentNode.removeChild(element);
   }
 
   function addClass (className, element) {
-
+    if (element.classList) {
+      return element.classList.add(className);
+    } else {
+      return element.className += ' ' + className;
+    }
   }
 
   function removeClass (className, element) {
-
+    if (element.classList) {
+      return element.classList.remove(className);
+    } else {
+      return element.className = element.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+    }
   }
 
-  function addAttribute (attributeName, attributeValue, element) {
-
+  function addAttribute (element, attributes) {
+    for (attr in attributes) {
+      return element.setAttribute(attr.name, attr.value);
+    }
   }
 
   function removeAttribute (attributeName, element) {
-
+    return element.removeAttribute(attributeName);
   }
 
   dom = {
-    creatElement: creatElement,
-    appendElement: appendElement,
+    addElement: addElement,
     removeElement: removeElement,
+    append: append,
+    prepend: prepend,
     addClass: addClass,
     removeClass: removeClass,
     addAttribute: addAttribute,
