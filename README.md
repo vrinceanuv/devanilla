d3d5d9e3045989fb82cffe4c7aee7ab968030071
vrinceanuv <vrinceanu.valentin@gmail.com>
2017-04-06 23:35:49 +0100

    Changes: - Added es6 support with babel - Added browserify for require support - Added npm scripts for now - Updated README.md

--- README.md
+++ README.md
@@ -5,5 +5,12 @@ We have created this library, because we know out there are lots of vanilla js i
 devanilla purpose is to ease your way when you search for dom elements or want to apply events to one or multiple elements, do ajax calls and so on.
 Below you can find a list of features and how you can use or library.
 
+How to make it run:
+- npm install
+- npm run babel
+- npm run browserify
+- npm start
+- type in browser: http://localhos:9000/
+
 Features:
 - coming soon -
--- modules/dom/script.js
+++ modules/dom/script.js
@@ -1,105 +1,80 @@
-(function () {
+/**
+ * Creates an element
+ * @param  {string} element
+ */
 
-  var dom;
+export const addElement = (element) => document.createElement(element)
 
-  /**
-   * Creates an element
-   * @param  {string} element
-   */
+/**
+ * Removes an element from DOM
+ * @param  {string} element
+ */
 
-  function addElement (element) {
-    return document.creatElement(element);
+export const removeElement = (element) => element.parentNode.removeChild(element)
+
+/**
+ * Adds an element inside another element as last child
+ * @param  {node} element
+ * @param  {node} parent
+ */
+
+export const append = (element, parent) => {
+  let parentNode = document.querySelector(parent);
+  return parentNode.appendChild(element)
+}
+
+/**
+ * Adds an element inside another element as first child
+ * @param  {node} element
+ * @param  {node} parent
+ */
+
+export const prepend = (element, parent) => parent.insertBefore(element, parent.firstChild)
+
+/**
+ * Adds a class to an element
+ * @param  {string} className
+ * @param  {node} element
+ */
+
+export const addClass = (className, element) => {
+  if (element.classList) {
+    return element.classList.add(className);
+  } else {
+    return element.className += ' ' + className;
   }
+}
 
-  /**
-   * Removes an element from DOM
-   * @param  {string} element
-   */
+/**
+ * Removes a class from an element
+ * @param  {string} className
+ * @param  {node} element
+ */
 
-  function removeElement (element) {
-    return element.parentNode.removeChild(element);
+export const removeClass = (className, element) => {
+  if (element.classList) {
+    return element.classList.remove(className);
+  } else {
+    return element.className = element.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
   }
+}
 
-  /**
-   * Adds an element inside another element as last child
-   * @param  {node} element
-   * @param  {node} parent
-   */
+/**
+ * Adds an attribute or more attributes to an element
+ * @param  {object} attributes
+ * @param  {node} element
+ */
 
-  function append (element, parent) {
-    return parent.appendChild(element);
-  }
+export const addAttribute = (element, attributes) => {
+  return attributes.map( function(attr) {
+    return element.setAttribute(attr.name, attr.value)
+  } )
+}
 
-  /**
-   * Adds an element inside another element as first child
-   * @param  {node} element
-   * @param  {node} parent
-   */
+/**
+ * Removes an attribute from element
+ * @param  {string} attributeName
+ * @param  {node} element
+ */
 
-  function prepend (element, parent) {
-    return parent.insertBefore(element, parent.firstChild);
-  }
-
-  /**
-   * Adds a class to an element
-   * @param  {string} className
-   * @param  {node} element
-   */
-
-  function addClass (className, element) {
-    if (element.classList) {
-      return element.classList.add(className);
-    } else {
-      return element.className += ' ' + className;
-    }
-  }
-
-  /**
-   * Removes a class from an element
-   * @param  {string} className
-   * @param  {node} element
-   */
-
-  function removeClass (className, element) {
-    if (element.classList) {
-      return element.classList.remove(className);
-    } else {
-      return element.className = element.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-    }
-  }
-
-  /**
-   * Adds an attribute or more attributes to an element
-   * @param  {object} attributes
-   * @param  {node} element
-   */
-
-  function addAttribute (element, attributes) {
-    for (attr in attributes) {
-      return element.setAttribute(attr.name, attr.value);
-    }
-  }
-
-  /**
-   * Removes an attribute from element
-   * @param  {string} attributeName
-   * @param  {node} element
-   */
-
-  function removeAttribute (attributeName, element) {
-    return element.removeAttribute(attributeName);
-  }
-
-  dom = {
-    addElement: addElement,
-    removeElement: removeElement,
-    append: append,
-    prepend: prepend,
-    addClass: addClass,
-    removeClass: removeClass,
-    addAttribute: addAttribute,
-    removeAttribute: removeAttribute
-  };
-
-  return dom;
-});
+export const removeAttribute = (attributeName, element) => element.removeAttribute(attributeName)
