c0c18eb4aa83bb846d64e2d0780e285c906f4b8f
Vrinceanu Valentin <vrinceanu.valentin@gmail.com>
2017-04-18 21:42:03 +0100

    Added jest, exporting modules by names in main.js

--- modules/main.js
+++ modules/main.js
@@ -1,2 +1,16 @@
-import { addElement, removeElement, append, prepend, addClass, removeClass, addAttribute, removeAttribute } from './dom/script'
-import { addEvent, removeEvent } from './events/script'
+import dom from './dom/script'
+import events from './events/script'
+
+//exporting dom modules
+export const addElement = dom.addElement;
+export const removeElement = dom.removeElement;
+export const append = dom.append;
+export const prepend = dom.prepend;
+export const addClass = dom.addClass;
+export const removeClass = dom.removeClass;
+export const addAttribute = dom.addAttribute;
+export const removeAttribute = dom.removeAttribute;
+
+//exporting events
+export const addEvent = events.addEvent;
+export const removeEvent = events.removeEvent;
