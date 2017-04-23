b3573603237dfc7c58538d8785ed30601ef64eb0
vrinceanuv <vrinceanu.valentin@gmail.com>
2017-04-23 18:25:53 +0100

    Added documentation for the events module

--- modules/events/script.js
+++ modules/events/script.js
@@ -1,3 +1,10 @@
+/**
+ * Adds an event to an element or to some elements
+ * @param  {string} eventType - The event type
+ * @param  {string} element - The element(s) to attach the events
+ * @param  {function} callback - The callback to execute when the event is fired
+ */
+
 export const addEvent = ( eventType, element, callback ) => {
   if (element.length > 1) {
     element.map( (elem) => elem.addEventListener( eventType, callback ) );
@@ -6,6 +13,13 @@ export const addEvent = ( eventType, element, callback ) => {
   }
 }
 
+/**
+ * Removes an event to an element or to some elements
+ * @param  {string} eventType - The event type
+ * @param  {string} element - The element(s) to attach the events
+ * @param  {function} callback - The callback that was executed when the event was fired
+ */
+ 
 export const removeEvent = (eventType, element, callback) => {
   if (element.length > 1) {
     element.map( (elem) => elem.removeEventListener( eventType, callback ) );
