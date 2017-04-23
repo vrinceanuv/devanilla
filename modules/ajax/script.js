f3403c8544b9eed4c9deb93c94a717453b64f485
vrinceanuv <vrinceanu.valentin@gmail.com>
2017-04-23 18:25:29 +0100

    Re-wrote the backbone of the ajax module

--- modules/ajax/script.js
+++ modules/ajax/script.js
@@ -1,19 +1,10 @@
-(function () {
-
-  var ajax;
-
-  function getData (url, data, callback) {
-
-  }
-
-  function postData (url, data, callback) {
-
-  }
-
-  ajax = {
-    doGet: doGet,
-    doPost: doPost
-  };
-
-  return ajax;
-});
+/**
+ * Does an ajax call
+ * @param  {string} url - The url for the ajax call
+ * @param  {string} type - The type of the call: get, post, put, delete
+ * @param  {function} callback - The callback after the call is done
+ * @param  {string} data - The data you want to send when doing the call
+ */
+export const ajaxCall = (url, type, callback, data) => {
+  // TODO ajax call
+}
