56d076d7d0b276eeddc24879f6c666204a3f7318
vrinceanuv <vrinceanu.valentin@gmail.com>
2017-04-27 06:49:49 +0100

    Wrapping request into promises

--- modules/ajax/script.js
+++ modules/ajax/script.js
@@ -1,52 +1,65 @@
 /**
- * Does an ajax getRequest
+ * Does an ajax get
  * @param  {string} url - The url for the ajax call
  * @param  {function} callback - The callback after the call is done
  */
-export const getRequest = (url, callback) => {
-  const request = new XMLHttpRequest();
+export const get = (url) => {
+  /* eslint-disable */
+  return new Promise(function(resolve, reject) {
+    const request = new XMLHttpRequest();
 
-  request.open('GET', url, true);
+    request.open('GET', url, true);
 
-  request.onload = function() {
-    if (request.status >= 200 && request.status < 400) {
-      callback(JSON.parse(request.responseText), request);
-    } else {
-      callback(request.status, request);
-    }
-  };
+    request.onload = function() {
+      if (request.status === 200 && request.status < 400) {
+        resolve(request.response);
+      } else {
+        reject(Error(request.statusText));
+      }
+    };
 
-  request.onerror = function() {
-    callback(request.status, request);
-  };
+    request.onerror = function() {
+      reject(Error('Network Error'));
+    };
 
-  request.send();
+    request.send();
+  });
+  /* eslint-enable */
 };
 
 /**
- * Does an ajax postRequest
+ * Does an ajax post
  * @param  {string} url - The url for the ajax call
  * @param  {function} callback - The callback after the call is done
  * @param  {object} data - The data that we send
  * @param  {string} headers - Optional headers that we send
  */
-export const postRequest = (url, callback, data, headers) => {
-  const request = new XMLHttpRequest();
+export const post = (url, data, headers) => {
+  /* eslint-disable */
+  return new Promise(function(resolve, reject) {
+    const request = new XMLHttpRequest();
 
-  request.open('POST', url, true);
+    request.open('POST', url, true);
 
-  if(headers) {
-    request.setRequestHeader(headers);
-  } else {
-    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-  }
+    if (headers) {
+      request.setRequestHeader(headers);
+    } else {
+      request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    }
 
-  request.onload = function() {
-    callback(request.status, request);
-  };
+    request.onload = function() {
+      if (request.status === 200 && request.status < 400) {
+        resolve(request.response);
+      } else {
+        reject(Error(request.statusText));
+      }
+    };
 
-  request.onerror = function() {
-    callback(request.status, request);
-  };
+    request.onerror = function() {
+      reject(Error('Network Error'));
+    };
 
+    request.send();
+  });
+  /* eslint-enable */
 };
