11adf48bb18801c1b9b8950742cd4f160ff73648
vrinceanuv <vrinceanu.valentin@gmail.com>
2017-04-27 05:35:46 +0100

    changed the export so it can be easier used

--- modules/main.js
+++ modules/main.js
@@ -1,15 +1,13 @@
 import * as dom from './dom/script';
 import * as events from './events/script';
 
-export const dev = {
-  addElement: dom.addElement,
-  removeElement: dom.removeElement,
-  append: dom.append,
-  prepend: dom.prepend,
-  addClass: dom.addClass,
-  removeClass: dom.removeClass,
-  addAttribute: dom.addAttribute,
-  removeAttribute: dom.removeAttribute,
-  addEvent: events.addEvent,
-  removeEvent: events.removeEvent,
-};
+export const addElement = dom.addElement,
+             removeElement = dom.removeElement,
+             append = dom.append,
+             prepend = dom.prepend,
+             addClass = dom.addClass,
+             removeClass = dom.removeClass,
+             addAttribute = dom.addAttribute,
+             removeAttribute = dom.removeAttribute,
+             addEvent = events.addEvent,
+             removeEvent = events.removeEvent;
